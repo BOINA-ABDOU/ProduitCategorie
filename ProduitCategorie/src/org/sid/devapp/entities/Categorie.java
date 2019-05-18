7a4e70f3ff801c15e01d3edc44c20457a2f5acd0
HOUSNIYA <HOUSNIYA@HOUSNIYA>
2019-05-18 16:07:18 +0200

    commit 2

---
+++ ProduitCategorie/src/org/sid/devapp/entities/Categorie.java
@@ -0,0 +1,10 @@
+package org.sid.devapp.entities;
+
+import java.io.Serializable;
+
+public class Categorie implements Serializable{
+	private Long idCategorie;
+	private String nomCategorie;
+	private String description;
+	
+}
